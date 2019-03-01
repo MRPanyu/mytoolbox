1a8e3094e636a55ba4c8c3447209786026ccb9e4
Panyu <mrpanyu@aliyun.com>
2019-03-01 15:10:33 +0800

    EscapeFileTool

---
+++ src/main/java/mrpanyu/mytoolbox/tools/escape/EscapeFileTool.java
@@ -0,0 +1,109 @@
+package mrpanyu.mytoolbox.tools.escape;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.tools.ant.util.Native2AsciiUtils;
+
+import mrpanyu.mytoolbox.framework.api.Action;
+import mrpanyu.mytoolbox.framework.api.Parameter;
+import mrpanyu.mytoolbox.framework.api.ParameterType;
+import mrpanyu.mytoolbox.framework.api.Tool;
+
+public class EscapeFileTool extends Tool {
+
+	@Override
+	public void initialize() {
+		// 初始化配置
+		setName("0202_escapeFile");
+		setDisplayName("转义文件：Base64/Properties");
+		setDescription("Base64/Properties文本转义工具");
+		setEnableProfile(false);
+		// 初始化参数
+		Parameter param = new Parameter("type", "转义类型");
+		param.setDescription("转义类型");
+		param.setType(ParameterType.ENUMERATION);
+		param.setEnumerationValues(Arrays.asList("Base64Encode", "Base64Decode", "Native2Ascii", "Ascii2Native"));
+		param.setValue("Base64Encode");
+		addParameter(param);
+
+		param = new Parameter("inputFile", "输入文件");
+		param.setDescription("输入文件");
+		param.setType(ParameterType.FILE);
+		addParameter(param);
+
+		param = new Parameter("outputFile", "输出文件");
+		param.setDescription("输出文件");
+		param.setType(ParameterType.FILE);
+		addParameter(param);
+
+		// 初始化动作
+		addAction(new Action("escape", "转义"));
+	}
+
+	@Override
+	public void performAction(String actionName) {
+		getUserInterface().clearMessages();
+		try {
+			if ("escape".equals(actionName)) {
+				String type = getParameter("type").getValue();
+				String inputFile = getParameter("inputFile").getValue();
+				String outputFile = getParameter("outputFile").getValue();
+				File inputF = new File(inputFile);
+				File outputF = new File(outputFile);
+				if (!inputF.isFile()) {
+					getUserInterface().writeErrorMessage("输入文件不存在！");
+					return;
+				}
+				if (StringUtils.isBlank(outputFile)) {
+					getUserInterface().writeErrorMessage("请选择输出文件！");
+					return;
+				}
+				if ("Base64Encode".equals(type)) {
+					byte[] data = FileUtils.readFileToByteArray(inputF);
+					String base64 = Base64.encodeBase64String(data);
+					FileUtils.writeStringToFile(outputF, base64, "ISO-8859-1");
+				} else if ("Base64Decode".equals(type)) {
+					String base64 = FileUtils.readFileToString(inputF, "ISO-8859-1");
+					byte[] data = Base64.decodeBase64(base64);
+					FileUtils.writeByteArrayToFile(outputF, data);
+				} else if ("Native2Ascii".equals(type)) {
+					List<String> lines = FileUtils.readLines(inputF, "UTF-8");
+					List<String> outputLines = new ArrayList<>(lines.size());
+					for (String line : lines) {
+						String outputLine = Native2AsciiUtils.native2ascii(line);
+						outputLines.add(outputLine);
+					}
+					FileUtils.writeLines(outputF, "UTF-8", outputLines);
+				} else if ("Ascii2Native".equals(type)) {
+					List<String> lines = FileUtils.readLines(inputF, "UTF-8");
+					List<String> outputLines = new ArrayList<>(lines.size());
+					for (String line : lines) {
+						String outputLine = Native2AsciiUtils.ascii2native(line);
+						outputLines.add(outputLine);
+					}
+					FileUtils.writeLines(outputF, "UTF-8", outputLines);
+				}
+				getUserInterface().writeInfoMessage("已写入输出文件。");
+			}
+		} catch (Exception e) {
+			getUserInterface().writeErrorMessage(e.getClass() + ":" + e.getMessage());
+		}
+	}
+
+	@Override
+	public void onParameterValueChange(String name) {
+		if ("inputFile".equals(name)) {
+			if (StringUtils.isBlank(getParameter("outputFile").getValue())) {
+				getParameter("outputFile").setValue(getParameter("inputFile").getValue() + ".out");
+				getUserInterface().refreshParameterValue("outputFile");
+			}
+		}
+	}
+
+}
--- src/main/java/mrpanyu/mytoolbox/tools/escape/EscapeTool.java
+++ src/main/java/mrpanyu/mytoolbox/tools/escape/EscapeTool.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.tools.ant.util.Native2AsciiUtils;
 
 import mrpanyu.mytoolbox.framework.api.Action;
 import mrpanyu.mytoolbox.framework.api.Parameter;
@@ -18,15 +19,16 @@ public class EscapeTool extends Tool {
 	public void initialize() {
 		// 初始化配置
 		setName("0201_escape");
-		setDisplayName("转义：URL/XML/JS/Base64文本转义");
+		setDisplayName("转义：URL/XML/JS/Base64");
 		setDescription("URL/XML/JS/Base64文本转义工具，支持Escape和Unescape");
 		setEnableProfile(false);
 		// 初始化参数
 		Parameter param = new Parameter("type", "转义类型");
 		param.setDescription("转义类型");
 		param.setType(ParameterType.ENUMERATION);
-		param.setEnumerationValues(Arrays.asList("URLEncode", "URLDecode", "EscapeXML", "UnescapeXML",
-				"EscapeJavaScript", "UnescapeJavaScript", "Base64Encode", "Base64Decode"));
+		param.setEnumerationValues(
+				Arrays.asList("URLEncode", "URLDecode", "EscapeXML", "UnescapeXML", "EscapeJavaScript",
+						"UnescapeJavaScript", "Native2Ascii", "Ascii2Native", "Base64Encode", "Base64Decode"));
 		param.setValue("URLEncode");
 		addParameter(param);
 		param = new Parameter("value", "原值");
@@ -57,6 +59,20 @@ public class EscapeTool extends Tool {
 					escapeValue = StringEscapeUtils.escapeJavaScript(value);
 				} else if ("UnescapeJavaScript".equals(type)) {
 					escapeValue = StringEscapeUtils.unescapeJavaScript(value);
+				} else if ("Native2Ascii".equals(type)) {
+					StringBuilder sb = new StringBuilder();
+					for (String line : value.split("\n", -1)) {
+						sb.append(Native2AsciiUtils.native2ascii(line)).append("\n");
+					}
+					sb.deleteCharAt(sb.length() - 1);
+					escapeValue = sb.toString();
+				} else if ("Ascii2Native".equals(type)) {
+					StringBuilder sb = new StringBuilder();
+					for (String line : value.split("\n", -1)) {
+						sb.append(Native2AsciiUtils.ascii2native(line)).append("\n");
+					}
+					sb.deleteCharAt(sb.length() - 1);
+					escapeValue = sb.toString();
 				} else if ("Base64Encode".equals(type)) {
 					escapeValue = Base64.encodeBase64String(value.getBytes("UTF-8"));
 				} else if ("Base64Decode".equals(type)) {
